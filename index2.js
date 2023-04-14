e36a5722dbb6ac9d99b81e3e04511a45a89d16d2
abeljlim <subrosian@hotmail.com>
2023-04-14 13:21:17 -0700

    removed tinkering code and comments

--- index2.js
+++ index2.js
@@ -14,11 +14,6 @@ const printPassTimes = function(passTimesArr) {
 nextISSTimesForMyLocation()
   .then(body => {
     printPassTimes(body); // callback returns undefined
-    return new Promise((resolve, reject) => reject(new Error("error message to catch"))); // works as intended - a new Promise passes in arguments to the callback function that are resolve and reject callbacks that can be used
-    //reject(new Error("error message to catch")); // doesn't work because we need the reject or resolve to come from a Promise
-  })
-  .then(() => {
-    console.log("Called after the previous then which returned undefined?"); // tinkering to see what happens when a promise is not returned; looks like a Promise being returned results in waiting for the Promise to be fulfilled before moving to the next "then(...)" being done whereas nothing and/or a non-promise being returned results in the next "then(...)" being done in a synchronous fashion
   })
   .catch(error => {
     console.log("It didn't work: ", error.message);
