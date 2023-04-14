cf6a2098e0f234f220a3912355177bd7c7a35b78
abeljlim <subrosian@hotmail.com>
2023-04-13 21:25:38 -0700

    Getting date and printing it out

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const timeConverter = function(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  return time;
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
   console.log(passTimes);
+  for (const passTime of passTimes) {
+    // e.g. Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
+    const unix_timestamp = passTime.risetime;
+    const formattedTime = new Date(unix_timestamp * 1000);
+
+    console.log(`Next pass at ${formattedTime} for ${passTime.duration} seconds!`);
+  }
 });
 
 // fetchISSFlyOverTimes({ latitude: 'abc', longitude: 999 }, (error, data) => {
