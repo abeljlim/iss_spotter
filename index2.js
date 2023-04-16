4d3c0b9c233e54c680a4ca5a09c108e20e0a372f
abeljlim <subrosian@hotmail.com>
2023-04-16 13:10:10 -0700

    Tinker stuff with promises to help understand the control flow and how Promises fit into the event loop model

--- index2.js
+++ index2.js
@@ -15,9 +15,35 @@ nextISSTimesForMyLocation()
   .then(body => {
     printPassTimes(body); // callback returns undefined
   })
+  .then(() => {
+    console.log("Called after the previous then which returned undefined?"); // tinkering to see what happens when a promise is not returned; looks like a Promise being returned results in waiting for the Promise to be fulfilled before moving to the next "then(...)" being done whereas nothing and/or a non-promise being returned results in the next "then(...)" being done in a synchronous fashion
+  })
   .catch(error => {
     console.log("It didn't work: ", error.message);
   });
+
+new Promise((resolve, reject) => {
+  console.log("Executed on main thread or not? 1"); // indeed is executed on the main thread
+  resolve();
+}).then(() => { // the rest of these "then(...)" function calls run
+  console.log("Executed on main thread or not? 2"); // this not executed on the main thread
+}).then(() => {
+  console.log("Executed on main thread or not? 3");
+}).then(() => {
+  console.log("Executed on main thread or not? 4");
+});
+console.log("Executed on main thread");
+setTimeout(() => {console.log("Executed via setTimeout(..., 0);")}, 0);
+new Promise((resolve, reject) => {
+  console.log("After setTimeout call; executed on main thread or not? 1"); // indeed is executed on the main thread
+  resolve();
+}).then(() => { // the rest of these "then(...)" function calls run
+  console.log("After setTimeout call; Executed on main thread or not? 2"); // this not executed on the main thread
+}).then(() => {
+  console.log("After setTimeout call; Executed on main thread or not? 3");
+}).then(() => {
+  console.log("After setTimeout call; Executed on main thread or not? 4");
+});
 // fetchMyIP()
 //   .then(fetchCoordsByIP) // callback is called with the arguments passed in from the resolve call of the promise function, so don't need to call the callback with arguments
 //   .then(fetchISSFlyOverTimes)
