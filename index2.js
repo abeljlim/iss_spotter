55ff5a3cf3a613f7cc5c891ed849846d98482a52
abeljlim <subrosian@hotmail.com>
2023-04-16 14:00:07 -0700

    Some more tinker with inner Promise

--- index2.js
+++ index2.js
@@ -14,8 +14,21 @@ const printPassTimes = function(passTimesArr) {
 nextISSTimesForMyLocation()
   .then(body => {
     printPassTimes(body); // callback returns undefined
-    return new Promise((resolve, reject) => reject(new Error("error message to catch"))); // works as intended - a new Promise passes in arguments to the callback function that are resolve and reject callbacks that can be used
+    
+    // return new Promise((resolve, reject) => reject(new Error("error message to catch"))); // works as intended - a new Promise passes in arguments to the callback function that are resolve and reject callbacks that can be used
     //reject(new Error("error message to catch")); // doesn't work because we need the reject or resolve to come from a Promise
+
+    // From watching "Syntax": If the value of the resolve callback is a Promise, then apparently that Promise is handled before going to the next "then(...)" callback
+    return new Promise((resolve, reject) => {
+      console.log("before inner Promise resolution");
+      resolve(new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log("inner Promise resolution");
+          resolve(undefined);
+        }, 1000);
+      console.log("after inner Promise resolution");
+      }));
+    }); 
   })
   .then(() => {
     console.log("Called after the previous then which returned undefined?"); // tinkering to see what happens when a promise is not returned; looks like a Promise being returned results in waiting for the Promise to be fulfilled before moving to the next "then(...)" being done whereas nothing and/or a non-promise being returned results in the next "then(...)" being done in a synchronous fashion
